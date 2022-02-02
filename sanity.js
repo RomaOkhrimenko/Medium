67439eead299082c06922795a5bffc89df4ed0c2
RomaOkhrimenko <roma.okhrimenko@gmail.com>
2022-02-02 19:59:48 +0200

    Medium

---
+++ sanity.js
@@ -0,0 +1,15 @@
+import {createImageUrlBuilder, createCurrentUserHook, createClient} from 'next-sanity'
+
+export const config = {
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    projectId: "8ztycusq",
+    apiVersion: "2021-03-25",
+
+    useCdn: process.env.NODE_ENV === "production",
+}
+
+export const sanityClient = createClient(config)
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source)
+
+export const useCurrentUser = createCurrentUserHook(config)
